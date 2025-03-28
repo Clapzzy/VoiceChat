ec66350cc866f3cd9a39235b626d59f28fb5b3fb
Clapzzy <martin.kurtew@gmail.com>
2025-03-29 01:19:16 +0200

    added mic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,74 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const audioContext = new AudioContext()
-  const gainNode = audioContext.createGain(); // Create a volume control node
-  gainNode.gain.value = 0.1; // Set volume to 50%
+  const [mute, setMute] = useState(true)
 
-  const oscillator = audioContext.createOscillator(); // Create a sound source
-  oscillator.connect(gainNode); // Connect sound → volume control
-  gainNode.connect(audioContext.destination); // Connect volume control → speakers
-  oscillator.start(); // Play sound
+  const audioContextRef = useRef()
+
+  const oscillatorRef = useRef()
+  const gainRef = useRef()
+
+  const microphoneStreamRef = useRef()
+  const micNodeRef = useRef()
+  const microphoneDevices = useRef([])
+  const currentMic = useState()
+
+  useEffect(() => {
+    audioContextRef.current = new AudioContext()
+
+    oscillatorRef.current = audioContextRef.current.createOscillator(); // Create a sound source
+    oscillatorRef.current.connect(audioContextRef.current.destination)
+    oscillatorRef.current.start()
+    audioContextRef.current.suspend()
+
+    navigator.mediaDevices.enumerateDevices()
+      .then((devices) => {
+        devices.forEach((device) => {
+          if (device.kind == "audioinput") {
+
+            if (device.deviceId == "default") {
+              currentMic.current = device
+            }
+            microphoneDevices.current.push(device)
+          }
+        })
+      })
+
+    return () => {
+      oscillatorRef.current.stop()
+      audioContextRef.current.close()
+    }
+  }, [])
+
+  useEffect(() => {
+
+    navigator.mediaDevices.getUserMedia({ audio: { deviceId: { exact: currentMic.current } }, video: false })
+      .then((mediaStream) => {
+        microphoneStreamRef.current = mediaStream
+        gainRef.current = audioContextRef.current.createGain(); // Create a volume control node
+        gainRef.current.gain.value = 1; // Set volume to 50%
+        micNodeRef.current = audioContextRef.current.createMediaStreamSource(microphoneStreamRef.current)
+
+        micNodeRef.current.connect(gainRef.current)
+        gainRef.current.connect(audioContextRef.current.destination)
+      })
+      .catch((err) => {
+        console.log("Got an error :", err)
+      })
+
+  }, [currentMic.current])
+
+  useEffect(() => {
+    if (mute == true) {
+      audioContextRef.current.suspend()
+    } else {
+      audioContextRef.current.resume()
+    }
+  }, [mute])
 
   return (
     <>
@@ -28,6 +84,11 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+        <button onClick={() => setMute((mute) => !mute)}>
+          {mute
+            ? (<div class='hidden' style={{ background: "#EE6C4D" }}>Disabled</div>)
+            : (<div className="" style={{ background: "#E0FBFC" }}>Enabled</div>)}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
