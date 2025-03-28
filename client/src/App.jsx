4578d8e10af6be0d27cf67153017129b626c3747
Clapzzy <martin.kurtew@gmail.com>
2025-03-28 17:39:25 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const audioContext = new AudioContext()
+  const gainNode = audioContext.createGain(); // Create a volume control node
+  gainNode.gain.value = 0.1; // Set volume to 50%
+
+  const oscillator = audioContext.createOscillator(); // Create a sound source
+  oscillator.connect(gainNode); // Connect sound → volume control
+  gainNode.connect(audioContext.destination); // Connect volume control → speakers
+  oscillator.start(); // Play sound
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
