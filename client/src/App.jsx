5f80dabc9bbdbf6845f98cba636433a21af4aed4
Clapzzy <martin.kurtew@gmail.com>
2025-03-29 15:25:32 +0200

    the mic finnaly works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,7 +7,6 @@ function App() {
 
   const audioContextRef = useRef()
 
-  const oscillatorRef = useRef()
   const gainRef = useRef()
 
   const microphoneStreamRef = useRef()
@@ -20,43 +17,43 @@ function App() {
   useEffect(() => {
     audioContextRef.current = new AudioContext()
 
-    oscillatorRef.current = audioContextRef.current.createOscillator(); // Create a sound source
-    oscillatorRef.current.connect(audioContextRef.current.destination)
-    oscillatorRef.current.start()
     audioContextRef.current.suspend()
 
     navigator.mediaDevices.enumerateDevices()
       .then((devices) => {
-        devices.forEach((device) => {
-          console.log(device)
-          if (device.kind == "audioinput") {
-
-            if (device.deviceId == "default") {
-              setCurrentMic(device)
-            }
-            console.log("setting up mic : ", device.label)
-            setMicrophoneDevices([...microphoneDevices, device])
-          }
-        })
+        const audioDevices = devices.filter((device) => device.kind === "audioinput")
+        setMicrophoneDevices(audioDevices)
+        if (!currentMic) {
+          setCurrentMic(0)
+        }
       })
 
     return () => {
-      oscillatorRef.current.stop()
       audioContextRef.current.close()
     }
   }, [])
 
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ audio: { deviceId: { exact: currentMic } }, video: false })
+    let mic = microphoneDevices[currentMic];
+    if (!mic) return
+
+    if (microphoneStreamRef.current) {
+      microphoneStreamRef.current.getTracks().forEach(track => track.stop());
+    }
+
+    navigator.mediaDevices.getUserMedia({ audio: { deviceId: { ideal: mic.deviceId } }, video: false })
       .then((mediaStream) => {
+        console.log("currently using mic :", mic.label)
         microphoneStreamRef.current = mediaStream
         gainRef.current = audioContextRef.current.createGain(); // Create a volume control node
-        gainRef.current.gain.value = 1; // Set volume to 50%
+        gainRef.current.gain.value = 3; // Set volume to 50%
         micNodeRef.current = audioContextRef.current.createMediaStreamSource(microphoneStreamRef.current)
-        console.log("using mic : ", currentMic)
 
         micNodeRef.current.connect(gainRef.current)
         gainRef.current.connect(audioContextRef.current.destination)
+
+        if (!mute) audioContextRef.current.resume()
+
       })
       .catch((err) => {
         console.log("Got an error :", err)
@@ -82,20 +79,20 @@ function App() {
         </button>
         <button onClick={() => setMute((mute) => !mute)}>
           {mute
-            ? (<div class='hidden' style={{ background: "#EE6C4D" }}>Disabled</div>)
-            : (<div className="" style={{ background: "#E0FBFC" }}>Enabled</div>)}
+            ? (<div style={{ background: "#EE6C4D" }}>Disabled</div>)
+            : (<div style={{ background: "#E0FBFC" }}>Enabled</div>)}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
         <div>
-          <select className='w-full h-10 ' value={currentMic} onChange={(event) => setCurre}>
-            {microphoneDevices.map((mic, index) => {
-              console.log(mic.label)
-              return (
-                <option value={index}>{mic.label}</option>
-              )
-            })}
+          <select className='w-full h-10 ' value={currentMic} onChange={(event) => setCurrentMic(event.target.value)}>
+            {
+              microphoneDevices.map((mic, index) => {
+                return (
+                  <option className='text-[#293241] bg-[#E0FBFC]' value={index}>{mic.label}</option>
+                )
+              })}
           </select>
         </div>
       </div>
