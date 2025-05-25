b8abee4368b2a98b4bbcc4f84596d81604bfd83c
Clapzzy <martin.kurtew@gmail.com>
2025-05-25 19:20:00 +0300

    added allowedHosts in vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    allowedHosts: ['martinkurtev.com'],
+    host: '0.0.0.0',
     port: 80
   }
 })
